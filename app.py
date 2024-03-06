ca7b8521578944f1bcf40375ad74cb1335c01d23
Wiritipon Thapkhan <81100736+zignalssss@users.noreply.github.com>
2024-03-06 17:00:15 +0000

    import req

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify ,request
 from flask_cors import CORS,cross_origin
 app = Flask(__name__)
 cors = CORS(app)
@@ -10,49 +10,41 @@ books=[
     {"id":3,"title":"Book 3","author":"Author 3"}
 ]
 @app.route("/")
-def Greet():
-    return "<p>Welcome to Book Management Systems</p>"
+@cross_origin()
+def hello_world():
+    return  "<h1>Hello_world</h1>"
 
 @app.route("/books",methods=["GET"])
+@cross_origin()
 def get_all_books():
     return jsonify({"books":books})
 
-@app.route("/books/<int:book_id>",methods=["GET"])
-def get_book(book_id):
-    book =  next(( b for b in books if b["id"]==book_id ),None)
-    if book:
-        return jsonify(book)
-    else:
-        return jsonify({"error":"Book not found"}),404
-
 @app.route("/books",methods=["POST"])
-def create_book():
-    data = request.get_json()
-    new_book={
-        "id":len(books)+1,
-        "title":data["title"],
-        "author":data["author"]
-    }
+@cross_origin()
+def insert_id_book():
+    new_book = request.get_json()
     books.append(new_book)
-    return jsonify(new_book),201
+    return jsonify({"books": books})
 
-@app.route("/books/<int:book_id>",methods=["PUT"])
-def update_book(book_id):
-    book = next((b for b in books if b["id"]==book_id),None)
-    if book:
-        data = request.get_json()
-        book.update(data)
-        return jsonify(book)
-    else:
-        return jsonify({"error":"Book not found"}),404
+@app.route("/books/<int:data_book>", methods=["PUT"])
+@cross_origin()
+def put_id_book(data_book):
+    for book in books:
+        if book["id"] == data_book:
+            updated_book = request.get_json()
+            book.update(updated_book)
+            return jsonify({"books": books}), 200
 
-@app.route("/books/<int:book_id>",methods=["DELETE"])
-def delete_book(book_id):
-    book = next((b for b in books if b["id"]==book_id),None)
-    if book:
-        books.remove(book)
-        return jsonify({"message":"Book deleted successfully"}),200
-    else:
-        return jsonify({"error":"Book not found"}),404
+    return jsonify({"error": "Book not found"}), 404
+
+@app.route("/books/<int:data_book>", methods=["DELETE"])
+@cross_origin()
+def delete_id_book(data_book):
+    for book in books:
+        if book["id"] == data_book:
+            books.remove(book)
+            return jsonify({"books": books}), 200
+    return jsonify({"error": "Book not found"}), 404
+
 if __name__ == "__main__":
-    app.run(host="0.0.0.0",port=5000,debug=True)
+    app.run(host="0.0.0.0", port=5000, debug=True, encoding='utf-8')
